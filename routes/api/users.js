ecd5bdadbbaca4bcd7ff8626d9b128caf01e7f99
Roman Klimenko <romaklimenko@gmail.com>
2019-04-19 20:53:37 +0200

    Close #9 кэш-сервис

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,13 +15,23 @@ const activityMapper = a => {
 }
 
 const getActivities = async (user, type) => {
-  const maxPageCount = 50
+  const activities = []
+  let limit = -1
+
+  if (process.env.DIRTY_CACHE_URL) {
+    const cache_url = new URL(process.env.DIRTY_CACHE_URL.replace('%username%', user.toLowerCase()))
+    const cached_response = await fetch(cache_url)
+    if (cached_response.status == 200) {
+      const cached_response_json = await cached_response.json()
+      activities.push(...cached_response_json[type])
+      limit = cached_response_json.limit
+    }
+  }
+
   let page = 1
   let pageCount = null
-  let originalPageCount = null
 
   const url = new URL(`https://d3.ru/api/users/${user}/${type}/`)
-  const activities = []
 
   while (pageCount === null || page <= pageCount) {
     url.searchParams.set('page', page)
@@ -35,8 +45,7 @@ const getActivities = async (user, type) => {
         originalPageCount: 0
       }
     }
-    pageCount = Math.min(pageCount || response.page_count, maxPageCount)
-    originalPageCount = originalPageCount || response.page_count
+    pageCount = pageCount || response.page_count
     if (response[type].length > 0 && response[type][0].user) {
       // TODO: плохо пахнет – нам нужно имя пользователя только чтобы проверить регистр
       user = response[type][0].user.login
@@ -44,18 +53,27 @@ const getActivities = async (user, type) => {
     else {
       console.log(response[type][0])
     }
-    activities.push(...response[type].map(activityMapper))
-    page++
+
+    const activities_to_push = response[type].map(activityMapper)
+      .filter(a => a.created !== undefined)
+      .filter(a => limit < 0 || a.created > limit)
+
+    if (activities_to_push.length === 0) {
+      page = pageCount + 1
+    }
+    else {
+      activities.push(...activities_to_push)
+      page++
+    }
   }
 
   activities.sort((a, b) => a.created < b.created ? 1 : -1)
 
   return {
-    activities: activities.filter(a => a.created !== undefined),
+    activities: activities,
     type: type,
     user: user,
-    pageCount: pageCount,
-    originalPageCount: originalPageCount
+    pageCount: pageCount
   }
 }
 
@@ -71,15 +89,6 @@ router.get('/', async (req, res) => {
     else {
       const posts = await getActivities(user, 'posts')
       const comments = await getActivities(user, 'comments')
-
-      if (posts.pageCount < posts.originalPageCount || comments.pageCount < comments.originalPageCount) {
-        if (posts.activities.length > comments.activities.length) {
-          comments.activities = comments.activities.filter(c => c.created >= posts.activities[posts.activities.length - 1].created)
-        }
-        else {
-          posts.activities = posts.activities.filter(p => p.created >= comments.activities[comments.activities.length - 1].created)
-        }
-      }
 
       if (posts.activities.length > 0) {
         user = posts.user
